2e979ee3526fef967c3ffecd47dfd3832e93d213
Thomas Lemberger <lembergerth@gmail.com>
2018-12-07 12:57:40 +0100

    Re-add Nullable tag at correct position

--- src/org/sosy_lab/llvm_j/Utils.java
+++ src/org/sosy_lab/llvm_j/Utils.java
@@ -30,6 +30,7 @@
 package org.sosy_lab.llvm_j;
 
 import com.sun.jna.Pointer;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.llvm_j.binding.LLVMLibrary;
 
 /** Util methods for the llvm parser. */
@@ -41,7 +42,7 @@ public final class Utils {
    * Converts the given {@link org.sosy_lab.llvm_j.binding.LLVMLibrary.LLVMBool LLVMBool} value to a
    * Java boolean.
    */
-  static boolean llvmBoolToJavaBool(LLVMLibrary.LLVMBool pBool) {
+  static boolean llvmBoolToJavaBool(LLVMLibrary.@Nullable LLVMBool pBool) {
     // LLVMBool is actually an int.
     // This is a wrong mapping done by JNA, so we have to convert the PointerType
     // to an integer value.
